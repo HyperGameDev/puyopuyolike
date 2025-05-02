5b7daaa28e45b08f6f29a15718f74bf855269f81
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2025-05-01 22:42:11 -0500

    pieces are areas and it's a problem

--- Components/component_movement.gd
+++ Components/component_movement.gd
@@ -5,18 +5,36 @@ class_name Component_Movement extends Timer
 const FALL_SPEED: float = .4
 var falling: bool = true
 
-var can_move_horizonally: bool = true
+var has_wall_on_right: bool = false
+var has_wall_on_left: bool = false
+
+var has_piece_on_right: bool = false
+var has_piece_on_left: bool = false
+var has_piece_on_down: bool = false
 
 func _ready() -> void:
 	object.grounded.connect(_on_grounded)
-	#object.walls.connect(_on_walled)
+	
+	object.unblocked_right.connect(_on_unblocked_right)
+	object.blocked_right.connect(_on_blocked_right)
+	
+	object.unblocked_left.connect(_on_unblocked_left)
+	object.blocked_left.connect(_on_blocked_left)
+	
+	object.unblocked_bottom.connect(_on_unblocked_bottom)
+	object.blocked_bottom.connect(_on_blocked_bottom)
+	
 	timeout.connect(_on_timeout)
 	start(.5)
 	
 	
 func _input(_event: InputEvent) -> void:
-	if can_move_horizonally:
-		object.position.x -= Input.get_axis("Right","Left")
+	if not has_wall_on_left:
+		if not has_piece_on_left:
+			object.position.x -= float(Input.is_action_pressed("Left"))
+	if not has_wall_on_right:
+		if not has_piece_on_right:
+			object.position.x += float(Input.is_action_pressed("Right"))
 	
 func _on_timeout():
 	if falling:
@@ -27,9 +45,32 @@ func _on_timeout():
 func _on_grounded() -> void:
 	falling = false
 	
-func _on_walled() -> void:
-	can_move_horizonally = false
+func _on_unblocked_right(is_walled: bool) -> void:
+	if is_walled:
+		has_wall_on_right = false
+	else:
+		has_piece_on_right = false
+func _on_blocked_right(is_walled: bool) -> void:
+	if is_walled:
+		has_wall_on_right = true
+	else:
+		has_piece_on_right = true
 	
+func _on_unblocked_left(is_walled: bool) -> void:
+	if is_walled:
+		has_wall_on_left = false
+	else:
+		has_piece_on_left = false
+func _on_blocked_left(is_walled: bool) -> void:
+	if is_walled:
+		has_wall_on_left = true
+	else:
+		has_piece_on_left = true
+	
+func _on_unblocked_bottom() -> void:
+	has_piece_on_down = false
+func _on_blocked_bottom() -> void:
+	has_piece_on_down = true
 	
 	
 	
--- Objects/area_piece.gd
+++ Objects/area_piece.gd
@@ -1,14 +1,29 @@
-class_name Piece extends Node3D
+class_name Piece extends Area3D
 
 signal grounded
-signal blocked_left
-signal blocked_right
+
+signal unblocked_left(is_walled:bool)
+signal blocked_left(is_walled:bool)
+
+signal unblocked_right(is_walled:bool)
+signal blocked_right(is_walled:bool)
+
+signal unblocked_bottom
+signal blocked_bottom
+
+@export var fallen: bool = false
 
 @export var piece_type: piece_types
-enum piece_types {PLAYER,GARBAGE,PRESET}
+enum piece_types {PLAYER,GARBAGE,PLACED,PRESET}
 
 @export var piece_color: piece_colors
 enum piece_colors {GARBAGE,RED,YELLOW,GREEN,BLUE,PURPLE}
+
+enum walled_state {NEITHER,LEFT,RIGHT}
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
 
 func _process(_delta: float) -> void:
 	if is_on_floor():
@@ -17,14 +32,75 @@ func _process(_delta: float) -> void:
 	check_sides()
 	
 func check_sides() -> void:
-	#if 
-	pass
+	#match has_piece_nearby():
+		
+	match detected_walled_state():
+		walled_state.NEITHER:
+			unblocked_left.emit(true)
+			unblocked_right.emit(true)
+		walled_state.LEFT:
+			blocked_left.emit(true)
+			unblocked_right.emit(true)
+		walled_state.RIGHT:
+			blocked_right.emit(true)
+			unblocked_left.emit(true)
 		
 func is_on_floor() -> bool:
 	return is_equal_approx(position.y,Globals.GROUND_POS)
 
-func is_on_wall() -> bool:
-	return is_equal_approx(position.x,Globals.WALL_L_POS) or is_equal_approx(position.x,Globals.WALL_R_POS)
+func detected_walled_state() -> walled_state:
+	if is_equal_approx(position.x,Globals.WALL_L_POS):
+		return walled_state.LEFT
+	elif is_equal_approx(position.x,Globals.WALL_R_POS):
+		return walled_state.RIGHT
+	else:
+		return walled_state.NEITHER
+		
+func _on_area_entered(area) -> void:
+	if piece_sees_on_right(area):
+		if fallen:
+			match area.piece_type:
+				Piece.piece_types.PLAYER:
+					area.blocked_left.emit(false)
+					print("player blocked on left")
+				Piece.piece_types.PLACED:
+					check_for_match()
+				_:
+					pass
+		else:
+			blocked_right.emit(false)
+			
+	if piece_sees_on_bottom(area):
+		if fallen:
+			check_for_match()
+		else:
+			blocked_bottom.emit()
+			
+func _on_area_exited(area) -> void:
+	if fallen:
+		match area.piece_type:
+			Piece.piece_types.PLAYER:	
+				if not piece_sees_on_right(area):
+					area.unblocked_left.emit(false)
+					print(name,": ",name," unblocked on left")
+			Piece.piece_types.PLACED:
+				check_for_match()
+			_:
+				pass
+	else:
+		unblocked_right.emit(false)
+	if not piece_sees_on_bottom(area):
+		unblocked_bottom.emit()
+		
+func piece_sees_on_right(area) -> bool:
+	return is_equal_approx(area.position.x,position.x + 1.)
 	
-func is_on_piece() -> bool:
-	return false
+func piece_sees_on_bottom(area) -> bool:
+	return is_equal_approx(area.position.y,position.y - 1.)
+
+#func piece_seen_by_left
+
+#func piece_seen_by_top
+
+func check_for_match() -> void:
+	pass

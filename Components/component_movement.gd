03aa9355303b9fb33b75888d020ae2a73c9ed6d9
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2025-05-02 17:54:08 -0500

    Movement improvement

--- Components/component_movement.gd
+++ Components/component_movement.gd
@@ -10,11 +10,16 @@ var top_piece: Node3D
 const FALL_SPEED: float = .4
 var falling: bool = true
 
+var walled_state: walled_states
+enum walled_states {NEITHER,LEFT,RIGHT}
+
 const MOVE_SPEED_HORIZ: float = 1.
 const BUTTON_HOLD_DELAY: float = .1
 var moving_left: bool = false
 var moving_right: bool = false
 var moving_down: bool = false
+
+var downward_movement_allowed: bool = true
 
 const MOVE_SPEED_ROTATE: float = .3
 var rotate_position: rotate_positions
@@ -41,12 +46,23 @@ func _ready() -> void:
 	
 	timer.timeout.connect(_on_timeout)
 	timer.start(.5)
+	
+	%Area_Main.area_entered.connect(_on_area_main_entered)
+	%Area_Main.area_exited.connect(_on_area_main_exited)
+	
+	%Area_Bottom.area_entered.connect(_on_area_bottom_entered)
 
 func _input(event) -> void:
 	if(event is InputEventKey and event.is_pressed()): #Ensure a key is pressed
 		rotational_movement()
 		directional_movement()
 	
+func _on_timeout():
+	if falling:
+		object.position.y -= FALL_SPEED
+	else:
+		timer.stop()
+		
 func rotational_movement() -> void:
 	get_current_bottom_piece_pos()
 	if Input.is_action_just_pressed("Button 1"):
@@ -85,77 +101,60 @@ func rotate_piece(direction: Vector3,new_position: rotate_positions):
 	rotate_position = new_position
 
 func directional_movement() -> void:
-	print("moving a direction")
-	if(Input.is_action_pressed("Left") and not moving_left):
-		moving_left=true
-		while(true):
-			if(not Input.is_action_pressed("Left")):break
-			object.position.x -= MOVE_SPEED_HORIZ
-			await get_tree().create_timer(BUTTON_HOLD_DELAY).timeout
-		moving_left=false
-	
-	if(Input.is_action_pressed("Right") and not moving_right):
-		moving_right=true
-		while(true):
-			if(not Input.is_action_pressed("Right")):break
-			object.position.x += MOVE_SPEED_HORIZ
-			await get_tree().create_timer(BUTTON_HOLD_DELAY).timeout
-		moving_right=false
-		
-	if(Input.is_action_pressed("Down") and not moving_down):
-		print("moving down")
-		moving_down = true
-		while(true):
-			if(not Input.is_action_pressed("Down")): break
-			
-			var downward_fall_speed: float = object.position.y - FALL_SPEED * 10
-			var tween: Tween = %DownwardTweens.create_tween()	
-			tween.set_ease(Tween.EASE_OUT)
-			tween.tween_property(object, "position:y", downward_fall_speed, .1)
-			
-			await get_tree().create_timer(BUTTON_HOLD_DELAY).timeout
-		moving_down = false
-	
-func _on_timeout():
 	if falling:
-		object.position.y -= FALL_SPEED
-	else:
-		timer.stop()
+		if not walled_state == walled_states.LEFT:
+			if(Input.is_action_pressed("Left") and not moving_left):
+				moving_left=true
+				while(true):
+					if(not Input.is_action_pressed("Left") or walled_state == walled_states.LEFT or not downward_movement_allowed):break
+					object.position.x -= MOVE_SPEED_HORIZ
+					await get_tree().create_timer(BUTTON_HOLD_DELAY).timeout
+				moving_left=false
 		
-enum walled_state {NEITHER,LEFT,RIGHT}
+		if not walled_state == walled_states.RIGHT:
+			if(Input.is_action_pressed("Right") and not moving_right):
+				moving_right=true
+				while(true):
+					if(not Input.is_action_pressed("Right") or walled_state == walled_states.RIGHT or not downward_movement_allowed):break
+					object.position.x += MOVE_SPEED_HORIZ
+					await get_tree().create_timer(BUTTON_HOLD_DELAY).timeout
+				moving_right=false
+			
+		if(Input.is_action_pressed("Down") and not moving_down) and downward_movement_allowed:
+			moving_down = true
+			while(true):
+				if(not Input.is_action_pressed("Down") or not falling or not downward_movement_allowed): break
+				
+				object.position.y -= FALL_SPEED
+				#var tween: Tween = %DownwardTweens.create_tween()	
+				#tween.set_ease(Tween.EASE_OUT)
+				#tween.tween_property(object, "position:y", downward_fall_speed, .1)
+				
+				await get_tree().create_timer(BUTTON_HOLD_DELAY/25).timeout
+			moving_down = false
+	
+	
+func _on_area_main_entered(area) -> void:
+	match area.name:
+		"Ground":
+			falling = false
+		"Wall_L":
+			walled_state = walled_states.LEFT
+		"Wall_R":
+			walled_state = walled_states.RIGHT
+		_:
+			pass
 
-func _process(_delta: float) -> void:
-	if is_on_floor():
-		#grounded.emit()
-		pass
-		
-	check_sides()
-	
-func check_sides() -> void:
-	#match has_piece_nearby():
-		
-	#match detected_walled_state():
-		#walled_state.NEITHER:
-			#unblocked_left.emit(true)
-			#unblocked_right.emit(true)
-		#walled_state.LEFT:
-			#blocked_left.emit(true)
-			#unblocked_right.emit(true)
-		#walled_state.RIGHT:
-			#blocked_right.emit(true)
-			#unblocked_left.emit(true)
-	pass
-		
-func is_on_floor() -> bool:
-	return is_equal_approx(position.y,Globals.GROUND_POS)
-
-func detected_walled_state() -> walled_state:
-	if is_equal_approx(position.x,Globals.WALL_L_POS):
-		return walled_state.LEFT
-	elif is_equal_approx(position.x,Globals.WALL_R_POS):
-		return walled_state.RIGHT
-	else:
-		return walled_state.NEITHER
-	
-	
-	
+func _on_area_main_exited(area) -> void:
+	match area.name:
+		"Wall_L":
+			walled_state = walled_states.NEITHER
+		"Wall_R":
+			walled_state = walled_states.NEITHER
+		_:
+			pass
+			
+func _on_area_bottom_entered(area) -> void:
+	if area.name == "Ground":
+		print("you are setting it up correctly")
+		downward_movement_allowed = false
